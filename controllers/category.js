82aa27e5b46b445836ebd9a2a1f0ae44e4d48baf
Dip Parmar <parmar.dpp@gmail.com>
2021-10-16 17:05:37 +0530

    feature: implemented update-read-delete-list for category

--- controllers/category.js
+++ controllers/category.js
@@ -14,6 +14,67 @@ const create = (req, res) => {
   });
 };
 
+const read = (req, res) => {
+  return res.json(req.category);
+};
+
+const update = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, data) => {
+    if (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: errorHandler(err),
+      });
+    }
+    return res.json(data);
+  });
+};
+
+const remove = (req, res) => {
+  const category = req.category;
+  category.remove((err, data) => {
+    if (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: errorHandler(err),
+      });
+    }
+    return res.json({
+      message: 'Category removed successfully',
+    });
+  });
+};
+
+const list = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: errorHandler(err),
+      });
+    }
+    return res.json(data);
+  });
+};
+
+const categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      if (err) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+          error: 'Category does not exist',
+        });
+      }
+    }
+    req.category = category;
+    next();
+  });
+};
+
 module.exports = {
   create,
+  read,
+  update,
+  remove,
+  list,
+  categoryById,
 };
--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { create } = require('../controllers/category');
+const { create, read, update, list, remove, categoryById } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const router = express.Router();
 
 router.post('/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.get('/:categoryId', read);
+router.put('/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
+router.delete('/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+
+router.get('/', list);
+
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 module.exports = router;
